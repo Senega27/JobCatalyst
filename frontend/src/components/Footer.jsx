e08811cbcadac51c2d9262d9b4bb1ddce48135bd
Senega27 <seneganagarajan27@gmail.com>
2025-04-17 19:03:17 +0530

    feature signup working

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  FaSquareXTwitter,
+  FaSquareInstagram,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa6";
+
+const Footer = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
+  return (
+    <>
+      <footer>
+        <div>
+          <img src="/job.png" alt="logo" />
+        </div>
+        <div>
+          <h4>Support</h4>
+          <ul>
+            <li>112/3(2) ABC streat, Anna Nagar,Chennai</li>
+            <li>senegameena@gmail.com</li>
+            <li>+91 9876567892</li>
+          </ul>
+        </div>
+
+        <div>
+          <h4>Quick Links</h4>
+          <ul>
+            <li to={"/"}>
+              <Link>Home</Link>
+            </li>
+            <li to={"/jobs"}>
+              <Link>Jobs</Link>
+            </li>
+            {isAuthenticated && (
+              <li>
+                <Link to={"/dashboard"}>Dashboard</Link>
+              </li>
+            )}
+          </ul>
+        </div>
+        <div>
+          <h4>Follow Us</h4>
+          <ul>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaSquareXTwitter />
+                </span>
+                <span>Twitter (X)</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaSquareInstagram />
+                </span>
+                <span>Instagram</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaYoutube />
+                </span>
+                <span>Youtube</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaLinkedin />
+                </span>
+                <span>LinkedIn</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      <div className="copyright">
+        &copy; CopyRight. All Rights Reserved 
+      </div>
+    </>
+  );
+};
+
+export default Footer;
